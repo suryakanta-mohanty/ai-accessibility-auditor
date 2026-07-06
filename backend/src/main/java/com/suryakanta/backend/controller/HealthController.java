068b08452aba93d34c008f38dbaf4b8424b98169
suryakanta-mohanty <suryakantamohanty855@gmail.com>
2026-07-06 22:08:59 +0530

    Added CORS configuration for frontend integration

--- backend/src/main/java/com/suryakanta/backend/controller/HealthController.java
+++ backend/src/main/java/com/suryakanta/backend/controller/HealthController.java
@@ -1,18 +1,17 @@
 package com.suryakanta.backend.controller;
 
+import com.suryakanta.backend.dto.HealthResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 public class HealthController {
 
     @GetMapping("/api/health")
-    public Map<String, String> healthCheck(){
-        return Map.of(
-                "status", "UP",
-                "message", "Backend running 🚀"
+    public HealthResponse healthCheck(){
+        return new HealthResponse(
+                "UP",
+                "Backend running 🚀"
         );
     }
 }
